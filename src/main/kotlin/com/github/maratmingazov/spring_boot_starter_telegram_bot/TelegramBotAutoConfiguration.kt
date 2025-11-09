7f44d3ca8d2b2eadd8e42b6f00289fdaf1d503f3
MaratMingazov <marat@miro.com>
2025-11-09 09:26:15 +0400

    Create TelegramBotAutoConfiguration

---
+++ src/main/kotlin/com/github/maratmingazov/spring_boot_starter_telegram_bot/TelegramBotAutoConfiguration.kt
@@ -0,0 +1,13 @@
+package com.github.maratmingazov.spring_boot_starter_telegram_bot
+
+import org.springframework.context.annotation.Configuration
+
+/**
+ * файл org.springframework.boot.autoconfigure.AutoConfiguration.imports
+ * Это ServiceLoader-механизм Spring Boot 3+, который заменил старый путь META-INF/spring.factories.
+ *
+ * Текущая конфигурация подключится в проект и создадутся объявленные бины.
+ */
+@Configuration
+class TelegramBotAutoConfiguration {
+}
