fb7f5e3151e4be0349029e25f4163d9d5f0b686c
MaratMingazov <marat@miro.com>
2025-11-17 14:25:36 +0400

    Add matcher strategy

---
+++ src/main/kotlin/com/github/maratmingazov/spring_boot_starter_telegram_bot/handler/DefaultRequestMappingsMatcherStrategy.kt
@@ -0,0 +1,29 @@
+package com.github.maratmingazov.spring_boot_starter_telegram_bot.handler
+
+import com.github.maratmingazov.spring_boot_starter_telegram_bot.RequestMapping
+
+class DefaultRequestMappingsMatcherStrategy(): RequestMappingsMatcherStrategy, Comparator<RequestMappingInfo> {
+    override fun postProcess(mappings: List<RequestMapping>) {
+    }
+
+    override fun isMatched(
+        telegramBotEvent: TelegramBotEvent,
+        mappingInfo: RequestMappingInfo
+    ): Boolean {
+        return false
+    }
+
+    override fun extractPatternVariables(
+        text: String,
+        mappingInfo: RequestMappingInfo
+    ): Map<String, String> {
+        return mapOf()
+    }
+
+    override fun compare(
+        o1: RequestMappingInfo?,
+        o2: RequestMappingInfo?
+    ): Int {
+        return 0
+    }
+}
---
+++ src/main/kotlin/com/github/maratmingazov/spring_boot_starter_telegram_bot/handler/RequestMappingsMatcherStrategy.kt
@@ -0,0 +1,23 @@
+package com.github.maratmingazov.spring_boot_starter_telegram_bot.handler
+
+import com.github.maratmingazov.spring_boot_starter_telegram_bot.RequestMapping
+
+/**
+ * У контроллера может быть несколько методов помеченных @BotRequest аннотацией
+ * Стратегия определяет какой из этих методов должен быть выбран
+ */
+interface RequestMappingsMatcherStrategy {
+
+    fun postProcess(mappings: List<RequestMapping>)
+
+    /**
+     * Проверяем может ли данный метод (mappingInfo содержит информацию о методе) обработать данный telegramEvent
+     */
+    fun isMatched(telegramBotEvent: TelegramBotEvent, mappingInfo: RequestMappingInfo): Boolean
+
+    /**
+     * В телеграмм сообщении (text) может быть написано например /start
+     * Нужно выташить все такие path
+     */
+    fun extractPatternVariables(text: String, mappingInfo: RequestMappingInfo): Map<String, String>
+}
