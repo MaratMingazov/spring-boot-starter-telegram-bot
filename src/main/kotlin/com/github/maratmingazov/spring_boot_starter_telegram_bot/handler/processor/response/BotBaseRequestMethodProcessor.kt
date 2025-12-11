116fc1e19a62438e67ea4cf7512f3a61eff9f420
MaratMingazov <marat@miro.com>
2025-12-11 13:04:22 +0400

    Update BaseRequestProcessot

--- src/main/kotlin/com/github/maratmingazov/spring_boot_starter_telegram_bot/handler/processor/response/BotBaseRequestMethodProcessor.kt
+++ src/main/kotlin/com/github/maratmingazov/spring_boot_starter_telegram_bot/handler/processor/response/BotBaseRequestMethodProcessor.kt
@@ -2,19 +2,33 @@ package com.github.maratmingazov.spring_boot_starter_telegram_bot.handler.proces
 
 import com.github.maratmingazov.spring_boot_starter_telegram_bot.api.TelegramBotRequest
 import com.pengrad.telegrambot.request.BaseRequest
+import org.slf4j.LoggerFactory
 import org.springframework.core.MethodParameter
 
 class BotBaseRequestMethodProcessor(): BotHandlerMethodReturnValueHandler {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(BotHandlerMethodReturnValueHandlerComposite::class.java)
+    }
+
     override fun supportsReturnType(returnType: MethodParameter): Boolean {
-        return false
+        val paramType = returnType.parameterType
+        return BaseRequest::class.java.isAssignableFrom(paramType)
     }
 
     override fun handleReturnValue(
-        returnValue: Any,
+        returnValue: Any?,
         returnType: MethodParameter,
         telegramBotRequest: TelegramBotRequest
     ): BaseRequest<*, *>? {
-        throw RuntimeException("")
+        val paramType = returnType.parameterType
+        if (BaseRequest::class.java.isAssignableFrom(paramType)) {
+            if (paramType.isInstance(returnValue)) {
+                return returnValue as BaseRequest<*, *>
+            } else {
+                logger.error("Current request is not of type ${paramType.canonicalName}")
+            }
+        }
+        return null
     }
 }
